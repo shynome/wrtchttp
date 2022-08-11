1327f712b18a43078be8af1568a015ddbdd68e39
shynome <shynome@gmail.com>
2022-08-11 10:16:46 +0800

    add wasm example

--- example/config/config.go
+++ example/config/config.go
@@ -1,3 +1,6 @@
+//go:build !js
+// +build !js
+
 package config
 
 const SignalerServer = "http://127.0.0.1:1338/signaler?server=q&password=b"
---
+++ example/config/config_js.go
@@ -0,0 +1,6 @@
+//go:build js && wasm
+// +build js,wasm
+
+package config
+
+const SignalerServer = "http://127.0.0.1:8080/signaler?server=q&password=b"
